4ce8614c44202dd18c6965c3091c5ebd82c0ccf1
Max <knyaz010official@gmail.com>
2025-09-10 15:04:54 +0300

    upgrade hero block

--- src/home/Hero/Hero.jsx
+++ src/home/Hero/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
 
     return (
         <div className='hero'>
-            <h1 className='hero__head'>УСАДЬБА КОМАРОВО В ВИТЕБСКЕ</h1>
+            <h1 className='hero__head'>УСАДЬБА КОМАРОВО В ВИТЕБСКЕ <span>Русская баня на дровах, джакузи и отдых на природе</span></h1>
             <Button size={'large'}>Забронировать</Button>
             <div className="hero__images">
                 <img src="/hero_image_1.webp" alt="" className="hero__item"/>
